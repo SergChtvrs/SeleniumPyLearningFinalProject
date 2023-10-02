bc27537f5bf001e776f0416f66f54a3bc9c6c608
Serg <31658916+SergChtvrs@users.noreply.github.com>
2023-10-02 15:53:51 +0300

    the method of switching between pages has been changed

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,10 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    link_login_page = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = MainPage(browser, link)
-    login_page = LoginPage(browser, link_login_page)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
